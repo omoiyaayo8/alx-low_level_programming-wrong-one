ee21fa635171048b527b517e99e4d732b9ccfcea
omoiyaayo8 <yusuf_a_m@outlook.com>
2023-08-10 17:10:39 -0700

    5-printf.c

---
+++ 5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * Main- Program that print string follow by new Line
+ *
+ * Return : 0 (success)
+ *
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
